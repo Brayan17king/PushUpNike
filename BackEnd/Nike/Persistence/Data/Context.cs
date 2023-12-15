62613eaa0973c1fe78c77fda2904cb1159d99cf2
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-12-14 19:04:51 -0500

    feat: :tada: New Project

---
+++ BackEnd/Nike/Persistence/Data/Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using System.Reflection;
+
+namespace Persistence.Data;
+
+public partial class Context : DbContext
+{
+    public Context()
+    {
+    }
+
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Ciudad> Ciudades { get; set; }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Direccion> Direcciones { get; set; }
+
+    public virtual DbSet<FormaPago> FormaPagos { get; set; }
+
+    public virtual DbSet<Orden> Ordenes { get; set; }
+
+    public virtual DbSet<OrdenPedido> OrdenPedidos { get; set; }
+
+    public virtual DbSet<Pago> Pagos { get; set; }
+
+    public virtual DbSet<Pais> Paises { get; set; }
+
+    public virtual DbSet<Pedido> Pedidos { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Rol> Rols { get; set; }
+
+    public virtual DbSet<TipoProducto> TipoProductos { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+     public virtual DbSet<UserRol> UserRols { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) // 2611
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
